73aab630a5d5560e7b6d798712249a064f876278
Kirti Shinde <kirtishinde3520@gmail.com>
2026-06-08 20:11:38 +0530

    fix : final fix

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 """
 This is a Flask application.
 It starts a web server and
-
-returns a JSON response on the home route.
+returns a JSON response on the route.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 # ------------------ 
 #  Create Flask app
 # ------------------
 app = Flask(__name__) 
 
+# -------------
+# FUNCTIONS
+# -------------
+# read  👀
+def read_todo():
+    with open("db.json", "r") as file:
+        return json.load(file)
+
+# create  ✍️
+def create_todo(data):
+    with open("db.json", "w") as file:
+        json.dump(data, file)
 
 # -------------
 # ROUTE ⏩
@@ -20,6 +32,44 @@ app = Flask(__name__)
 def home():    
     # return {"message": "Flask Server Running Successfully 🚀 "}
     return render_template("index.html")
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return read_todo()["notes"]
+
+@app.route("/todos", methods=["POST"])
+def create_new_todo():
+
+    body = request.json
+    all_notes = read_todo()
+
+    # 1️⃣ read db,json and notes list
+    id = len(all_notes["notes"]) + 1
+    latest_note = {**body, "id" : id}
+    all_notes = read_todo()
+
+    # 2️⃣ append to notes list 
+    all_notes["notes"].append(latest_note)
+
+    # 3️⃣ write to db.json
+    create_todo(all_notes)
+    return {"message": "Todo created successfully ✅"}
+
+@app.route("/todo/<int:tid>", methods=["DELETE"])
+def remove_todo(tid):
+    # 1️⃣ read todos
+    all_notes = read_todo()
+
+    # 2️⃣ remove todo from notes
+    result = []
+    for item in all_notes["notes"]:
+        if item["id"] != tid:
+            result.append(item)
+    all_notes["notes"] = result
+
+    # 3️⃣ write to db.json
+    create_todo(all_notes)
+    return {"message": "Todo deleted successfully ✅"}
 
 # -------------
 # SERVER 🌍 
---
+++ test.py
@@ -0,0 +1,29 @@
+# brands = ["dell", "hp", "apple"]
+# brands.pop(1)
+# brands.remove("hp")
+# print(brands)
+
+# brands = [
+#     {"id": 1, "task": "dell", },
+#     {"id": 2, "task": "hp", },
+#     {"id": 3, "task": "apple", }
+# ]
+# print(brands[1]["task"])
+# brands.pop(1)
+# print(brands)
+
+
+# brands = [
+#     {"id": 1, "task": "dell", },
+#     {"id": 5, "task": "msi", },
+#     {"id": 2, "task": "hp", },
+#     {"id": 3, "task": "apple", }
+# ]
+# result = []
+# for item in brands :
+#     if item['id'] != 5:
+#         result.append(item)
+
+# print(result)
+
+
